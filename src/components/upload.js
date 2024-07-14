f51e960510bf75becf88a270ca31d62d8bbe0ff0
Rajat Verma <96742104+Rajat-eng@users.noreply.github.com>
2024-07-14 16:21:06 +0530

    Update upload.js

--- src/components/upload.js
+++ src/components/upload.js
@@ -21,7 +21,7 @@ export const Upload = ({ setText, setLoading }) => {
     formData.append("file", file);
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:8000/upload", {
+      const response = await fetch("https://ocr-backend-2c28.onrender.com/upload", {
         method: "POST",
         body: formData,
       });
